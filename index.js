8f553d3e30bb48f03b9d3302eb6a8a8bf2732fd6
Dandi Anastasa <dandianastasa12@gmail.com>
2024-07-09 00:10:51 +0700

    prepare basic configuration

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var cors = require('cors');
+var bodyParser = require('body-parser');
+var multer = require('multer');
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
